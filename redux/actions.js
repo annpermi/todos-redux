abfc561bc33952b5e0ab7be3134e0b58e7baa072
annpermi <perminova.anna85@gmail.com>
2021-11-09 09:57:58 -0500

    first commit

---
+++ redux/actions.js
@@ -0,0 +1,10 @@
+// const actionTypes = {
+// {type: 'todos/todoAdded', payload: todoText},
+// {type: 'todos/todoToggled', payload: todoId},
+// {type: 'todos/colorSelected, payload: {todoId, color}},
+// {type: 'todos/todoDeleted', payload: todoId},
+// {type: 'todos/allCompleted'},
+// {type: 'todos/completedCleared'},
+// {type: 'filters/statusFilterChanged', payload: filterValue},
+// {type: 'filters/colorFilterChanged', payload: {color, changeType}}
+// }
