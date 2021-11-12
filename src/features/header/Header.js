289eaae52ebc7668d0a2c7daec75c3f371f7a2d1
annpermi <perminova.anna85@gmail.com>
2021-11-12 11:40:42 -0500

    added loading for input

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -4,36 +4,41 @@ import { saveNewTodo } from '../todos/todosSlice'
 
 const Header = () => {
   const [text, setText] = useState('')
+  const [status, setStatus] = useState('idle')
   const dispatch = useDispatch()
 
   const handleChange = (e) => setText(e.target.value)
 
-  const handleKeyDown = (e) => {
+  const handleKeyDown = async (e) => {
     const trimmedText = text.trim()
     // If the user pressed the Enter key:
-    if (
-      /* e.key === 'Enter' */
-      e.which === 13 &&
-      trimmedText
-    ) {
+    if (/* e.key === 'Enter' */ e.which === 13 && trimmedText) {
       // Dispatch the "todo added" action with this text
       // dispatch({ type: 'todos/todoAdded', payload: trimmedText })
-      dispatch(saveNewTodo(trimmedText))
+      setStatus('loading')
+      await dispatch(saveNewTodo(trimmedText))
       // And clear out the text input
       setText('')
+      setStatus('idle')
     }
   }
+
+  let isLoading = status === 'loading'
+  let placeholder = isLoading ? '' : 'What needs to be done?'
+  let loader = isLoading ? <div className="loader" /> : null
 
   return (
     <header className="header">
       <input
         className="new-todo"
-        placeholder="What needs to be done?"
+        placeholder={placeholder}
         value={text}
         autoFocus={true}
         onKeyDown={handleKeyDown}
         onChange={handleChange}
+        disabled={isLoading}
       />
+      {loader}
     </header>
   )
 }
--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -4,7 +4,7 @@ import { StatusFilters } from '../filters/filtersSlice'
 
 const initialState = {
   status: 'idle',
-  entities: [],
+  entities: {},
 }
 
 // function nextTodoId(todos) {
@@ -13,6 +13,7 @@ const initialState = {
 // }
 
 export default function todosReducer(state = initialState, action) {
+  const todo = action.payload
   switch (action.type) {
     case 'todos/todoAdded': {
       // Can return just the new todos array - no extra object around it
@@ -23,58 +24,71 @@ export default function todosReducer(state = initialState, action) {
         //   text: action.payload,
         //   completed: false,
         // },
-        entities: [...state.entities, action.payload],
+        entities: {
+          ...state.entities,
+          [todo.id]: todo,
+        },
       }
     }
     case 'todos/todoToggled': {
+      const todoId = action.payload
+      const todo = state.entities[todoId]
       return {
         ...state,
-        entities: state.entities.map((todo) => {
-          if (todo.id !== action.payload) {
-            return todo
-          }
-
-          return {
+        entities: {
+          ...state.entities,
+          [todoId]: {
             ...todo,
             completed: !todo.completed,
-          }
-        }),
+          },
+        },
       }
     }
     case 'todos/colorSelected': {
       const { color, todoId } = action.payload
+      const todo = state.entities[todoId]
       return {
         ...state,
-        entities: state.entities.map((todo) => {
-          if (todo.id !== todoId) {
-            return todo
-          }
-
-          return {
+        entities: {
+          ...state.entities,
+          [todoId]: {
             ...todo,
             color,
-          }
-        }),
+          },
+        },
       }
     }
     case 'todos/todoDeleted': {
+      const newEntities = { ...state.entities }
+      delete newEntities[action.payload]
       return {
         ...state,
-        entities: state.entities.filter((todo) => todo.id !== action.payload),
+        entities: newEntities,
       }
     }
     case 'todos/allCompleted': {
+      const newEntities = { ...state.entities }
+      Object.values(newEntities).forEach((todo) => {
+        newEntities[todo.id] = {
+          ...todo,
+          completed: true,
+        }
+      })
       return {
         ...state,
-        entities: state.entities.map((todo) => {
-          return { ...todo, completed: true }
-        }),
+        entities: newEntities,
       }
     }
     case 'todos/completedCleared': {
+      const newEntities = { ...state.entities }
+      Object.values(newEntities).forEach((todo) => {
+        if (todo.completed) {
+          delete newEntities[todo.id]
+        }
+      })
       return {
         ...state,
-        entities: state.entities.filter((todo) => !todo.completed),
+        entities: newEntities,
       }
     }
     case 'todos/todosLoading': {
@@ -84,10 +98,14 @@ export default function todosReducer(state = initialState, action) {
       }
     }
     case 'todos/todosLoaded': {
+      const newEntities = {}
+      action.payload.forEach((todo) => {
+        newEntities[todo.id] = todo
+      })
       return {
         ...state,
         status: 'idle',
-        entities: action.payload,
+        entities: newEntities,
       }
     }
     default:
@@ -146,10 +164,14 @@ export function saveNewTodo(text) {
   }
 }
 
-export const selectTodos = (state) => state.todos.entities
+const selectTodoEntities = (state) => state.todos.entities
+
+export const selectTodos = createSelector(selectTodoEntities, (entities) =>
+  Object.values(entities)
+)
 
 export const selectTodoById = (state, todoId) => {
-  return selectTodos(state).find((todo) => todo.id === todoId)
+  return selectTodoEntities(state)[todoId]
 }
 
 export const selectTodoIds = createSelector(
